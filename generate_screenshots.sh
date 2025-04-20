698869b82473754688e98eef3992df32138a3781
Justin Hendrick <justinjhendrick@gmail.com>
2025-04-20 10:49:59 -0700

    add forgotten files

---
+++ generate_screenshots.sh
@@ -0,0 +1,26 @@
+set -e
+
+rebble build
+
+rebble wipe
+rebble kill
+rebble install --emulator aplite
+rebble screenshot screenshot_aplite.png
+
+rebble wipe
+rebble kill
+rebble install --emulator basalt
+rebble screenshot screenshot_basalt.png
+
+rebble wipe
+rebble kill
+rebble install --emulator diorite
+rebble screenshot screenshot_diorite.png
+
+rebble wipe
+rebble kill
+rebble install --emulator emery
+rebble screenshot screenshot_emery.png
+
+rebble wipe
+rebble kill
